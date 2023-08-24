ef9f6ae5c37fb0fb5cc35d29b1c7faddacaee1aa
Dam450 <68165752+dam450@users.noreply.github.com>
2023-08-24 01:12:12 -0300

    ♻️ convert getCityByNameService to typescript

--- src/services/getCityByNameService.ts
+++ src/services/getCityByNameService.ts
@@ -1,8 +1,17 @@
 import { api } from "./api";
 
-export async function getCityByNameService(name) {
+export interface ICityProps {
+  id: string
+  name: string
+  longitude: number
+  latitude: number
+}
+
+export async function getCityByNameService(name: string): Promise<ICityProps | null> {
   try {
     const { data } = await api.get(`/weather?q=${name}`);
+
+    console.log("data: ", data)
 
     const city = {
       id: data.id,
